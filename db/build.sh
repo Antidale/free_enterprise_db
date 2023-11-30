c7caafc8093b3af2841c1daa72d329c1e15c9d79
antidale <antidale@gmail.com>
2023-11-29 20:37:13 -0800

    more tournament schema changes, update setup scripts

--- db/build.sh
+++ db/build.sh
@@ -7,12 +7,14 @@ else
     createdb $FE_PGDATABASE -U $FE_PGUSER -h $FE_PGHOST    
 fi
 
-psql -h $FE_PGHOST  -d $FE_PGDATABASE -U $FE_PGUSER -f ./scripts/schema-def.sql
+psql -h $FE_PGHOST  -d $FE_PGDATABASE -U $FE_PGUSER -f ./scripts/create-schema.sql
+
+psql -h $FE_PGHOST  -d $FE_PGDATABASE -U $FE_PGUSER -f ./scripts/create-tables.sql
 
 psql -h $FE_PGHOST -d $FE_PGDATABASE -U $FE_PGUSER -c "\copy import.boss_data FROM './data/boss_scaling_stats.csv' with (format csv,header true, delimiter ',');"
 
 psql -h $FE_PGHOST -d $FE_PGDATABASE -U $FE_PGUSER -c "\copy import.equipment_data FROM './data/equipment_stats.csv' with (format csv, header true, delimiter ',');"
 
-psql -h $FE_PGHOST -d $FE_PGDATABASE -U $FE_PGUSER -f ./scripts/normalize-import.sql
+psql -h $FE_PGHOST -d $FE_PGDATABASE -U $FE_PGUSER -f ./scripts/normalize-import-data.sql
 
 psql -h $FE_PGHOST -d $FE_PGDATABASE -U $FE_PGUSER -f ./scripts/tournament-schema.sql
--- db/scripts/tournament-schema.sql
+++ db/scripts/tournament-schema.sql
@@ -2,22 +2,27 @@ create schema if not exists tournament;
 
 create table if not exists tournament.tournaments(
     id serial primary key,
-    guild_id text,
-    tracking_channel_id text,
-    tracking_message_id text,
-    tournament_name text,
-    registration_start timestamp with time zone,
-    registration_end timestamp with time zone,
+    guild_id text not null,
+    guild_name text not null,
+    tracking_channel_id text not null,
+    tracking_message_id text not null,
+    role_id text not null,
+    tournament_name text not null,
+    registration_start timestamptz,
+    registration_end timestamptz,
+    UNIQUE(guild_id, tournament_name)
 );
 
-create table if not exists tournament.entrant(
-    id serial primary key
-    user_id text,
-    user_name text,
+create table if not exists tournament.entrants(
+    id serial primary key,
+    user_id text UNIQUE,
+    user_name text NOT NULL,
+    pronouns text DEFAULT ''
 );
 
-create table if not exists tournament.entry(
-    tournament_id int references tournaments(id)
-    etrant_id int references entrants(id)
-    registered_on timestamp 
+create table if not exists tournament.registrations(
+    tournament_id int references tournament.tournaments(id),
+    entrant_id int references tournament.entrants(id),
+    user_name_alias text,
+    registered_on timestamp DEFAULT now()
 );
